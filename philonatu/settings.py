4026c9123f7b179c5c9c34aea3d553e8d7a47131
philonatu <philonatu@gmail.com>
2021-04-28 10:41:53 +0900

    add static_root

--- philonatu/settings.py
+++ philonatu/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files/')
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 STATIC_DIR = [
     os.path.join(BASE_DIR, 'static'),
